f8713fcc5dcfb3c16eec36a92f820c2d53e5e0e3
makkalot <makkalot@gmail.com>
2012-04-02 22:24:00 +0300

    add code to search and pull polls from hnews

--- lib/poll.py
+++ lib/poll.py
@@ -1,6 +1,15 @@
 from bs4 import BeautifulSoup
 import urllib2
 import re
+import time
+from urlparse import urljoin
+
+
+DOMAIN_NAME = "http://news.ycombinator.com"
+MAIN_URL = "http://news.ycombinator.com/newest"
+FIND_ITEM_RE = "^item\?id\=(\d+)$"
+HN_ITEM_STR = "item?id=%s"
+
 
 def get_html(url):
 	resp = urllib2.urlopen(url)
@@ -31,6 +40,82 @@ def get_answer_pack(url):
 	title = get_poll_title(html)
 	answs = get_answers(html)
 	return {"title":title,"answers":answs}
+
+def find_page_links(html):
+	soup = BeautifulSoup(html)
+ 	urls = filter(lambda a: not a is None, 
+							map(lambda td: td.a if td.find_all('a') else None,
+								soup.find_all("td",{"class":"title"})))
+
+	return urls
+
+def find_more_link(html):
+	"""
+	Finds the 'more' link in the hackernews page
+	"""
+ 	urls = find_page_links(html)
+	if not urls:
+		return None
+
+	return urls[-1].get('href')
+
+def find_poll_urls(urls):
+	"""
+	Finds the poll urls in given url list
+	The list is a soup list not normal one
+	"""
+	flist = []
+	for url in urls:
+		text = url.text.strip().lower()
+		if text.startswith('poll'):
+			#print "POLL : ",text
+			flist.append(url.get('href'))
+
+	return flist
+
+def get_poll_process_data(urls):
+	"""
+	Traverses the given urls and extracts
+	from them poll data if any and returns
+	back a list of it.
+	"""
+	poll_data = []
+	for url in urls:
+		try:
+			poll_res = get_answer_pack(urljoin(DOMAIN_NAME,url))
+			if not poll_res['answers']:
+				continue
+			print "POLL : %s "%poll_res
+			#sometimes they get angry when pull lots of data
+			time.sleep(1)
+			poll_data.append(poll_res)
+		except Exception,ex:
+			print "EXception : ",str(ex)
+			time.sleep(1)
+			continue
+		
+	return poll_data
+
+def getn_polls(n=10):
+	"""
+	Goes to hackernews and pulls the only polls
+	"""
+	search_html = get_html(MAIN_URL)
+	poll_data = get_poll_process_data(find_poll_urls(find_page_links(search_html)))
+	if len(poll_data) == n:
+		return poll_data 
+	link = find_more_link(search_html)
+	print link
+	while True:
+		search_html = get_html(urljoin(DOMAIN_NAME,link))
+		poll_data.extend(get_poll_process_data(find_poll_urls(find_page_links(search_html))))
+		if len(poll_data) >= n:
+			return poll_data
+		link = find_more_link(search_html)
+		print "NEXT 10: ",link
+
+	return poll_data
+
 
 if __name__ == "__main__":
 	#h = get_html("http://news.ycombinator.com/item?id=3771286")
